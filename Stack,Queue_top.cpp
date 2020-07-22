8727ef3218f9f031a6eb7daad9c7013d7a0c337b
kimnahee1529 <65105035+kimnahee1529@users.noreply.github.com>
2020-07-22 23:55:17 +0900

    Create Stack,Queue_top.cpp

---
+++ Stack,Queue_top.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+vector<int> solution(vector<int> heights) {
+    vector<int> answer;
+    stack<int> S;
+    for(int i=0; i<heights.size(); i++){
+        S.push(heights.at(i));
+        for(int j=i-1; j>=0; j--){
+            S.push(heights.at(j));
+            if(S.top()>heights.at(i)){
+                answer.push_back(j+1);
+                S.pop();
+                break;
+            }
+            else
+                S.pop();
+        }
+        S.pop();
+        if(answer.size()!=i+1)
+            answer.push_back(0);
+    }
+    return answer;
+}
+//https://programmers.co.kr/learn/courses/30/lessons/42588?language=cpp
